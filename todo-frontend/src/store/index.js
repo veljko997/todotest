ef336186f4d2ba4add6cac091682a657477a0b4f
veljko <veljkoroanda7@gmail.com>
2019-12-10 13:28:31 +0100

    Promenjeno

--- todo-frontend/src/store/index.js
+++ todo-frontend/src/store/index.js
@@ -4,7 +4,9 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    baseUrl:'http://localhost:8080/todotest/todo-backend'
+  },
   mutations: {},
   actions: {},
   modules: {}
